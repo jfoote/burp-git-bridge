923f024e3f1aa8f197e95da379841f4efd033657
Jonathan Foote <jmfoote@loyola.edu>
2015-04-19 12:34:54 -0700

    started comment pass

--- burp_and_rally.py
+++ burp_and_rally.py
@@ -1,3 +1,51 @@
+'''
+#
+# Chorus plugin for Burp Suite Pro
+#
+
+The Chorus plugin lets Burp users store and share findings and other Burp 
+items via git. Users can right-click supported items in Burp to send them to
+Chorus (and an underlying git repo), and use the Chorus tab to send items
+back to their respective Burp tools.
+
+This code is a PoC. Right now only Repeater and Scanner are supported.
+Stay tuned for a more polished version, or better yet consider sending 
+me a pull request. Thanks for checking it out.
+
+
+Jonathan Foote 
+jmfoote@loyola.edu
+https://github.com/jfoote/burp-chorus
+2015-04-19
+
+
+## Design
+
+The entire plugin is implemented in this single file. Ugly but convenient. The
+file is broken into sections. Each section contains multiple classes. The 
+sections are as follows:
+
+### Entry Point: 
+
+Implements BurpExtender, the entry point for the plugin.
+
+### Logging
+
+Objects that coordinate changes to the Git repo and in-Burp UI. Logging is 
+divided into objects that support git functionality (GitLog) and objects that 
+support in-Burp functionality (GuiLog). These two parts are wrapped into a 
+single class (Log) that is used by the UI components.
+
+### UI
+
+Swing UI Components. Bad. No fun.
+
+### Burp Interop
+
+Objects that are used to store user data as it is passed to 
+Burp callbacks (addScanIssue, etc.)
+'''
+
 from burp import IBurpExtender, ITab, IHttpListener, IMessageEditorController, IContextMenuFactory, IScanIssue, IHttpService, IHttpRequestResponse
 from java.awt import Component
 from java.awt.event import ActionListener
